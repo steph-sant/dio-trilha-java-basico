81b137b37413a285a3b4c42b70997f382b0f770e
steph-sant <stephaniesantt07@gmail.com>
2024-06-28 12:09:12 -0300

    Atualizando exemplos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,69 @@
+public class Operadores {
+   public static void main(String[] args) {
+
+    //Classe Operadores.java 
+    //String nome = "STEPHANIE";
+    //int idade = 24;
+    //double peso = 43.8;
+    //char sexo = 'F';
+    //boolean doadorOrgao = false;
+    //Date dataNascimento = new Date();
+
+    //operadores aritméticos: + (adição), - (subtração), * (multiplicação), / (divisão)
+    //double soma = soma = 10.5 + 15.7;
+    //int subtracao = 113 - 25;
+    //int multiplicacao = 20 * 7;
+    //int divisao = 15 / 3;
+    //int modulo = 18 % 3;
+    //double resultado = (10 * 7) + (20/4);
+    
+    //operador de adição (+) quando utilizado com variáveis do tipo texto, realiza a "concatenação de textos".
+    //String nomeCompleto = "LINGUAGEM" + "JAVA";
+    // System.out.println(nomeCompleto);
+
+    // String concatenacao  ="?";
+
+    // concatenacao = 1+1+1+"1";
+
+    // System.out.println(concatenacao);
+
+    // concatenacao = 1+"1"+1+1;
+    
+    // System.out.println(concatenacao);
+
+    // concatenacao = 1+"1"+1+"1";
+    
+    // System.out.println(concatenacao);
+
+    // concatenacao = "1"+1+1+1;
+
+    // System.out.println(concatenacao);
+    
+    // concatenacao = "1"+(1+1+1);
+
+    // System.out.println(concatenacao);
+
+    
+    //x repeticao, incremento e decremento
+    //int numero = 5;
+    //numero++;
+    //numero = numero +1;
+    //System.out.println(numero);
+    //System.out.println( ++ numero);
+
+    //numero--;
+    //System.out.println(numero);
+    //boolean variavel = true;
+    //variavel = !variavel;
+    //System.out.println(variavel);
+
+    //condicao ternaria
+    //int a, b;
+    //a = 5;
+    //b = 6;
+    //String resultado = a==b ?"verdadeiro" : "falso";
+    //System.out.println(resultado);
+    
+
+   }
+}
